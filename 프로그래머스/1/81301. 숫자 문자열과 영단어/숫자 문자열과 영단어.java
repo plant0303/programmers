b3b50fa7b464b4eddb9e78e5b94f75498c2156c6
seominhee <154911154+plant0303@users.noreply.github.com>
2025-06-23 16:56:58 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.70 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,45 +1,12 @@
 class Solution {
     public int solution(String s) {
-    if (s.contains("one")) {
-      s = s.replace("one", "1");
+        
+    String[] words = {"zero", "one", "two", "three", "four",
+                          "five", "six", "seven", "eight", "nine"};
+    
+    for(int i = 0; i < words.length; i++){
+        s = s.replaceAll(words[i], String.valueOf(i));
     }
-
-    if (s.contains("two")) {
-      s = s.replace("two", "2");
-    }
-
-    if (s.contains("three")) {
-      s = s.replace("three", "3");
-    }
-
-    if (s.contains("four")) {
-      s = s.replace("four", "4");
-    }
-
-    if (s.contains("five")) {
-      s = s.replace("five", "5");
-    }
-
-    if (s.contains("six")) {
-      s = s.replace("six", "6");
-    }
-
-    if (s.contains("seven")) {
-      s = s.replace("seven", "7");
-    }
-
-    if (s.contains("eight")) {
-      s = s.replace("eight", "8");
-    }
-
-    if (s.contains("nine")) {
-      s = s.replace("nine", "9");
-    }
-
-    if (s.contains("zero")) {
-      s = s.replace("zero", "0");
-    }
-
     int answer = 0;
     answer = Integer.parseInt(s);
     return answer;
